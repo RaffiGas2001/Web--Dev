5d2e86071b93ad9fa0a75cd6603255a175406766
RaffiGas2001 <117839601+RaffiGas2001@users.noreply.github.com>
2022-11-13 23:41:56 +0400

    Add files via upload

---
+++ Web--Dev/script.js
@@ -0,0 +1,39 @@
+let header_3 = document.querySelector("#header3")
+
+let click_event1 = document.querySelector(".click-event1")
+let event_p1 = document.querySelector(".event-p1")
+let header3_1_1 = document.querySelector(".header3-1-1")
+
+let click_event2 = document.querySelector(".click-event2")
+let event_p2 = document.querySelector(".event-p2")
+let header3_1_2 = document.querySelector(".header3-1-2")
+
+let click_event3 = document.querySelector(".click-event3")
+let event_p3 = document.querySelector(".event-p3")
+let header3_2_1 = document.querySelector(".header3-2-1")
+
+let click_event4 = document.querySelector(".click-event4")
+let event_p4 = document.querySelector(".event-p4")
+let header3_2_2 = document.querySelector(".header3-2-2")
+
+click_event1.addEventListener("click",function(){
+    header3_1_1.style.height = 400+"px"
+    event_p1.innerText = "Really boy law she unable her sister.Feet you off its like like six.Among sex are leave law built now.In built table in an rapid blush.Merits behind on afraid or warmly.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum animi consequatur aperiam, fugit vitae praesentium veniam voluptates illum, cumque neque nesciunt voluptate eaque? Voluptas ipsa repellat quibusdam quae. Recusandae, magnam."
+
+})
+
+click_event2.addEventListener("click",function(){
+    header3_1_2.style.height = 400+"px"
+    event_p2.innerText = "Really boy law she unable her sister.Feet you off its like like six.Among sex are leave law built now.In built table in an rapid blush.Merits behind on afraid or warmly.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum animi consequatur aperiam, fugit vitae praesentium veniam voluptates illum, cumque neque nesciunt voluptate eaque? Voluptas ipsa repellat quibusdam quae. Recusandae, magnam."
+})
+
+click_event3.addEventListener("click",function(){
+    header3_2_1.style.height = 400+"px"
+    event_p3.innerText = "Really boy law she unable her sister.Feet you off its like like six.Among sex are leave law built now.In built table in an rapid blush.Merits behind on afraid or warmly.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum animi consequatur aperiam, fugit vitae praesentium veniam voluptates illum, cumque neque nesciunt voluptate eaque? Voluptas ipsa repellat quibusdam quae. Recusandae, magnam."
+})
+
+
+click_event4.addEventListener("click",function(){
+    header3_2_2.style.height = 400+"px"
+    event_p4.innerText = "Really boy law she unable her sister.Feet you off its like like six.Among sex are leave law built now.In built table in an rapid blush.Merits behind on afraid or warmly.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum animi consequatur aperiam, fugit vitae praesentium veniam voluptates illum, cumque neque nesciunt voluptate eaque? Voluptas ipsa repellat quibusdam quae. Recusandae, magnam."
+})
